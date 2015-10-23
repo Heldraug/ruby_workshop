2041dd7b35ef1f86afcfe1b41bb9697d8810a0a6
Gerardo Galindez <ggalindez@nearsoft.com>
2015-10-22 21:29:44 -0500

    Twitter client refactor

--- 05_twitter.rb
+++ 05_twitter.rb
@@ -9,25 +9,36 @@ class TwitterClient
   OAUTH_KEY = '38909975-Pov2SpqASfpKNCt5kKYxcjlpBmRFU6zQ0riwdSIg9'
   OAUTH_SECRET = 'DR8L7qqtnpSOQQ9nFU9H3a0oqvbF25z7uO5UpKh0pg'
 
+  BASE_URI = 'https://api.twitter.com/1.1'
+
   def initialize
     consumer = OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, { :site => "https://api.twitter.com", :scheme => :header })
     token_hash = { :oauth_token => OAUTH_KEY, :oauth_token_secret => OAUTH_SECRET }
-    @token = OAuth::AccessToken.from_hash(consumer, token_hash)
+    @client = OAuth::AccessToken.from_hash(consumer, token_hash)
+  end
+
+  def query_api(method, path)
+    JSON.parse(@client.request(method, "#{BASE_URI}#{path}").body)
   end
 
   def home_timeline
-    JSON.parse(@token.request(:get, "https://api.twitter.com/1.1/statuses/home_timeline.json").body)
+    query_api(:get, '/statuses/home_timeline.json')
   end
 
   def user_timeline
-    JSON.parse(@token.request(:get, "https://api.twitter.com/1.1/statuses/user_timeline.json").body)
+    query_api(:get, '/statuses/user_timeline.json')
   end
 
   def retweets
-    JSON.parse(@token.request(:get, "https://api.twitter.com/1.1/statuses/retweets_of_me.json").body)
+    query_api(:get, '/statuses/retweets_of_me.json')
   end
 
   def post(status)
-    JSON.parse(@token.request(:post, "https://api.twitter.com/1.1/statuses/update.json", {status: status}).body)
+    query_api(:post, '/statuses/update.json')
   end
 end
+
+twitter = TwitterClient.new
+pp twitter.home_timeline
+pp twitter.user_timeline
+pp twitter.retweets
